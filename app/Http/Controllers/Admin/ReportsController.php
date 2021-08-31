72478fdee69a160aab403562863955f1b174ffe8
fajarprayoga <fajarprayoga23@gmail.com>
2021-08-31 10:44:57 +0800

    laporan

--- app/Http/Controllers/Admin/ReportsController.php
+++ app/Http/Controllers/Admin/ReportsController.php
@@ -15,10 +15,30 @@ use App\Traits\TraitModel;
 class ReportsController extends Controller
 {
     use TraitModel;
-public function reportSubHumas(){
-        return view ('admin.reports.reportSubHumas');
+
+
+    public function reportSubHumas(){
+        return view ('admin.reports.subHumas');
+        // return view ('admin.reports.reportSubHumas', compact('tickets'));
     }
-public function reportSubDistribusi(){
-        return view ('admin.reports.reportSubDistribusi');
+
+    public function reportSubHumasProses(Request $request)
+    {
+        $tickets = Ticket::whereYear('created_at', '=', $request->year)->whereMonth('created_at', '=', $request->month)->with(['action', 'customer', 'category','dapertement'])->get();
+        return view ('admin.reports.reportSubHumas', compact('tickets'));
     }
+
+    public function reportSubDistribusi(){
+        // return view ('admin.reports.reportSubDistribusi');
+        return view ('admin.reports.subDistribusi');
+    }
+
+    public function reportSubDistribusiProses(Request $request)
+    {
+        $tickets = Ticket::whereYear('created_at', '=', $request->year)->whereMonth('created_at', '=', $request->month)->with(['action', 'customer', 'category','dapertement'])->get();
+        // return view ('admin.reports.reportSubHumas', compact('tickets'));
+        // $tickets = Ticket::whereYear('created_at', '=', $request->year)->whereMonth('created_at', '=', $request->month)->with('customer')->get();
+        return view ('admin.reports.reportSubDistribusi', compact('tickets'));
+    }
+
 }
